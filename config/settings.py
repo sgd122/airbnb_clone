21d0050a8a747b0f57cf72845e8eb9e12702d170
[도울] 성기동 <dirrksdl@naver.com>
2020-01-09 22:26:45 +0900

    회원가입시 이메일전송

--- config/settings.py
+++ config/settings.py
@@ -141,3 +141,11 @@ AUTH_USER_MODEL = "users.User"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 MEDIA_URL = "/media/"
+
+# Email Configuration(mailgun)
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "sgd122@sandbox392bc88bada5441493004f5e52f36262.mailgun.org"
